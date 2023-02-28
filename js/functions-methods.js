3a4212e07efb27b59670119a0c7576b4a41fbf21
aekari <ashleighekari@gmail.com>
2023-02-28 08:46:35 -0800

    Added comments to js/functions-methods.js and css/style.css

--- js/functions-methods.js
+++ js/functions-methods.js
@@ -1,29 +1,33 @@
 
-
+// Create a global string variable called text and assign it a value of 'the sum is'
 var text = 'The sum is ';
+
+// Create a function called addIt that takes in to variables within its parameters, numA and numB. Locally within the function called addIt, there is a variable called total that is assigned to the sum of numA and numB. It then concatenates the value of the variable 'text' and the integer 'total'
 function addIt(numA, numB){
 		var total = numA + numB;
 		console.log(text + total);
 }
 
-//addIt(8,8);
-//addIt(10,10);
+// Here, we are calling the function addIt and passing values through it. It consoles "The sum is 20."
+addIt(10,10);
 
-var Ashleigh = {
-	arms: 2,
-	isSmart: true,
-	hairColor: 'black',
-	slogan: function(what){
-		console.log(what + ' is awesome');
+
+var Ashleigh = {			// the Ashleigh object has the following variables
+	arms: 2,				// a variable called arms that takes an int datatype
+	isSmart: true,			// a variable called isSmart that takes a boolean datatype
+	hairColor: 'black',		// a variable called hairColor that takes a string datatype
+	slogan: function(what){	// a method that takes a variable called "what" and does the following:
+		console.log(what + ' is awesome');	// concatenates the variable 'what' and ' is awesome', and prints both to the console.
 	}
 }
 
-Ashleigh.slogan('JavaScript');
-Ashleigh.hairColor = 'blue';
 
-console.log(Ashleigh.hairColor);
+Ashleigh.slogan('JavaScript');		// the 'what' variable is assigned a string: 'JavaScript'
+Ashleigh.hairColor = 'blue';		// reassigning the value of hairColor from 'black' to 'blue'
 
-//in the example above, 'slogan' is a method on the Asheigh object.
+console.log(Ashleigh.hairColor);	// Ashleigh's hair is now blue
+
+//in the example above, 'slogan' is a method on the Ashleigh object.
 //don't call a function before it's created!
 //booleans are not placed in quotations. If they were, they would instead be strings
 
@@ -39,11 +43,6 @@ function addIt(numA, numB){
 addIt(8,8);
 addIt(10,10);
 */
-
-
-
-
-
 
 /*
 functions addIt(){
